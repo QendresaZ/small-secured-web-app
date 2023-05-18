a717ca7e43ee0d28f50c803e54b39b1bab083a56
QendresaZ <qz40092@ubt-uni.net>
2023-05-18 10:32:48 +0200

    init

---
+++ src/main/java/com/example/smallsecuredwebapp/SmallSecuredWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.smallsecuredwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallSecuredWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallSecuredWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smallsecuredwebapp/SmallSecuredWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smallsecuredwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallSecuredWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
