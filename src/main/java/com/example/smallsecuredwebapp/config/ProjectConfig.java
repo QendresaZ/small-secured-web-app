af2c49442cf3bfb45bedcb661700041de913ddd6
QendresaZ <qz40092@ubt-uni.net>
2023-05-19 14:30:41 +0200

    register the AuthenticationProvider within the config class

--- src/main/java/com/example/smallsecuredwebapp/config/ProjectConfig.java
+++ src/main/java/com/example/smallsecuredwebapp/config/ProjectConfig.java
@@ -1,12 +1,21 @@
 package com.example.smallsecuredwebapp.config;
 
+import com.example.smallsecuredwebapp.service.AuthenticationProviderService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter{
+
+    private final AuthenticationProviderService authenticationProvider;
+
+    public ProjectConfig(AuthenticationProviderService authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -16,5 +25,11 @@ public class ProjectConfig {
     @Bean
     public SCryptPasswordEncoder sCryptPasswordEncoder() {
         return new SCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(
+                authenticationProvider);
     }
 }
