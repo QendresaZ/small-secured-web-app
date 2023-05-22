9884e686e075435cc46c2ccf564b13785237a222
QendresaZ <qz40092@ubt-uni.net>
2023-05-22 15:07:46 +0200

    modified

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT IGNORE INTO `spring_security_app`.`user` (`id`, `username`, `password`, `algorithm`) VALUES ('1', 'john', '$2a$10$xn3LI/AjqicFYZFruSwve.681477XaVNaUQbr1gioaWPn4t1KsnmG', 'BCRYPT');
+INSERT IGNORE INTO `spring_security_app`.`user` (`id`, `username`, `password`, `algorithm`) VALUES ('1', 'john', '$2a$10$xn3LI/AjqicFYZFruSwve.681477XaVNaUQbr1gioaWPn4t1KsnmG','BCRYPT');
 
 INSERT IGNORE INTO `spring_security_app`.`authority` (`id`, `name`, `user`) VALUES ('1', 'READ', '1');
 INSERT IGNORE INTO `spring_security_app`.`authority` (`id`, `name`, `user`) VALUES ('2', 'WRITE', '1');
