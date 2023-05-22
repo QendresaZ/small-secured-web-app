e8ea4f140371de8a58684cb76a794b4acf049580
QendresaZ <qz40092@ubt-uni.net>
2023-05-22 14:21:37 +0200

    config class modified

--- src/main/java/com/example/smallsecuredwebapp/config/ProjectConfig.java
+++ src/main/java/com/example/smallsecuredwebapp/config/ProjectConfig.java
@@ -3,19 +3,13 @@ package com.example.smallsecuredwebapp.config;
 import com.example.smallsecuredwebapp.service.AuthenticationProviderService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter{
-
-    private final AuthenticationProviderService authenticationProvider;
-
-    public ProjectConfig(AuthenticationProviderService authenticationProvider) {
-        this.authenticationProvider = authenticationProvider;
-    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -28,8 +22,11 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter{
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(
-                authenticationProvider);
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .defaultSuccessUrl("/main", true);
+
+        http.authorizeRequests()
+                .anyRequest().authenticated();
     }
 }
